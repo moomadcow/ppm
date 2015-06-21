ab051e4a417a7c0c780777983e4b76b588b44c0c
Steve Mu <stevemu@gmail.com>
2015-06-21 00:00:18 -0700

    Header logo and basic menu invoking button.

--- nav-main.php
+++ nav-main.php
@@ -1,6 +1,8 @@
 
 <header class="site-header">
   <div class="content-container">
-    <div class="container"></div>
+    <div class="container">
+      <div class="logo-ppm"><a href="/"></a></div><a class="nav-switch"><i class="fa fa-bars"></i></a>
+    </div>
   </div>
 </header>
