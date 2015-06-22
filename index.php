93b72c9b0f97be5b9ecf38d6ac6c211b5c90e26a
Steve Mu <stevemu@gmail.com>
2015-06-21 20:56:54 -0700

    Start putting together index page layout.

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 <section id="featured_articles" class="featured-articles">
-  <div class="content-container clearfix"><?php query_posts( array( 'tag' => 'featured', 'posts_per_page' => 3));
+  <div class="content-container clearfix"><?php query_posts( array('posts_per_page' => 4));
 if ( have_posts() ) while ( have_posts() ) : the_post();
-get_template_part( 'featured', get_post_format() );
+
+	get_template_part( 'featured', get_post_format() );
+
 endwhile;
 wp_reset_query();
  ?>
@@ -14,13 +16,20 @@ wp_reset_query();
       <!-- list of new articles-->
       <div id="ajax-load-content">
         <div class="articles-list">
-          <h1 class="title-latest"><strong>Featured Articles</strong></h1><?php $exclude_ID1 = get_category_id('news');
-$exclude_tag1 = get_tag_id('featured');
+          <h1 class="title-latest"><strong>Featured Articles</strong></h1><?php $args = array(
+	'posts_per_page' => 4
+	);
+$ids = array();
+$products = new WP_Query($args);
+if ( $products->have_posts() ) :
+    while ( $products->have_posts() ) : $products->the_post();
+       $ids[] = $post->ID;
+    endwhile;
+endif;
+
 query_posts( array(
-	'posts_per_page' => 5,
-	'tag__not_in' => $exclude_tag1,
-	'category__not_in' => $exclude_ID1,
-	'paged' => get_query_var( 'paged' ),
+	'post__not_in' => $ids,
+	'paged' => get_query_var( 'paged' )
 	));
 
 if ( have_posts() ) while ( have_posts() ) : the_post();
