9df0522fae5067cf39979302d85535416d8c7e17
Steve Mu <stevemu@gmail.com>
2015-06-24 21:15:06 -0700

    Footer complete.

--- footer.php
+++ footer.php
@@ -2,22 +2,8 @@
 <footer>
   <div class="content-container">
     <div class="container">
-      <nav class="social-nav"><a href="https://www.facebook.com/RallyHealth" target="_blank" title="Facebook" class="icons"><i class="fa fa-facebook"></i><span>Facebook</span></a><a href="https://twitter.com/rally_health" target="_blank" title="Twitter" class="icons"><i class="fa fa-twitter"></i><span>Twitter</span></a><a href="http://www.linkedin.com/company/rally-health" target="_blank" title="LinkedIn" class="icons"><i class="fa fa-linkedin"></i><span>LinkedIn</span></a></nav>
-      <div class="disclaimer">
-        <p>Rally complies with HIPAA, the Federal law that protects the privacy and security of health information.</p>
-      </div>
-      <div class="seals">
-        <div class="seal-hipaa"></div>
-        <div class="seal-ncqa"></div><a href="//privacy.truste.com/privacy-seal/Rally-Health-Solutions,-Inc-/validation?rid=3a07b23f-1765-47d0-8929-cefd80b4b67c" target="_blank" title="TRUSTe online privacy certification" class="seal-truste"></a>
-      </div>
-      <p>Rally Health, Inc. &copy; 2015 | All Rights Reserved</p>
+      <p class="copyright">Pixel Pushing Monkey &copy; 2015</p>
     </div>
   </div>
-</footer><a data-action="close" data-category="modal" data-label="close modal" class="modal-overlay ga-events"></a>
-<div class="modal universal"><a data-action="close" data-category="modal" data-label="close modal" class="close ga-events"><i class="fa fa-close"></i></a>
-  <div class="container">
-    <div class="content"></div>
-    <div class="overlay"></div>
-  </div>
-</div><?php wp_footer(); ?></body>
+</footer><?php wp_footer(); ?></body>
 </html>
